69c1eea119b80ac58a57cf941874e4a3bd525f39
Phil Crump <phil.crump@goonhilly.org>
2021-11-28 10:24:11 +0000

    Update devtools.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const webpack = require('webpack');
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
-const config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
-    },
-    devtool: '#source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src/'),
-                exclude: '/node_modules/',
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'es2016', 'es2017'],
-                }
-            }
-        ]
-    }
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, 'src/'),
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
 };
-
-module.exports = config;
